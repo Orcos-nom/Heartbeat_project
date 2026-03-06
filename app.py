a53a9d2a45f38f8978c03433939f0e01e80158ec
Orcos-nom <shadabahmadkhan27@gmail.com>
2026-03-06 13:55:19 +0530

    commit

--- app.py
+++ app.py
@@ -1,6 +1,5 @@
 import os
-from datetime import datetime
-from flask import Flask, render_template, request, jsonify
+from flask import Flask, render_template, request
 
 app = Flask(__name__)
 
@@ -8,33 +7,21 @@ UPLOAD_FOLDER = "recordings"
 os.makedirs(UPLOAD_FOLDER, exist_ok=True)
 
 @app.route("/")
-def home():
+def index():
     return render_template("index.html")
 
-
 @app.route("/upload", methods=["POST"])
-def upload_audio():
-    if "audio_data" not in request.files:
-        return jsonify({"status": "error", "message": "No audio file received"}), 400
+def upload():
 
-    audio_file = request.files["audio_data"]
+    audio = request.files['audio_data']
 
-    if audio_file.filename == "":
-        return jsonify({"status": "error", "message": "Empty filename"}), 400
+    filepath = os.path.join(UPLOAD_FOLDER, "recorded.wav")
 
-    timestamp = datetime.now().strftime("%Y%m%d_%H%M%S")
-    filename = f"recording_{timestamp}.wav"
-    filepath = os.path.join(UPLOAD_FOLDER, filename)
+    audio.save(filepath)
 
-    audio_file.save(filepath)
+    print("Audio saved:", filepath)
 
-    return jsonify({
-        "status": "success",
-        "message": "Audio saved",
-        "file": filename
-    })
-
+    return "saved"
 
 if __name__ == "__main__":
-    port = int(os.environ.get("PORT", 5000))  # required for Render
-    app.run(host="0.0.0.0", port=port)
+    app.run(debug=True)
